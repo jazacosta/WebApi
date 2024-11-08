295222e60332977558bff1f3511cf6ec1b32dd1f
jazacosta <136267410+jazacosta@users.noreply.github.com>
2024-11-08 12:08:52 -0300

    added validations with some rules

--- Infraestructura/Validations/CreateValidation.cs
+++ Infraestructura/Validations/CreateValidation.cs
@@ -5,4 +5,33 @@ namespace Infrastructure.Validations;
 
 public class CreateValidation : AbstractValidator<CreateCustomerDTO>
 {
+    public CreateValidation()
+    {
+        RuleFor(create => create.FirstName)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Name can only contain letters, dots or spaces.")
+            .Length(3, 40);
+
+        RuleFor(create => create.LastName)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Name can only contain letters, dots or spaces.")
+            .Length(3, 40);
+
+        RuleFor(create => create.Email)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("The email must have an at symbol");
+
+        RuleFor(create => create.Phone)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("The phone number must have at least 10 numbers");
+
+        RuleFor(create => create.BirthDate)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("The date format has to be yyyy/mm/dd");
+    }
 }
--- Infraestructura/Validations/UpdateValidation.cs
+++ Infraestructura/Validations/UpdateValidation.cs
@@ -5,4 +5,38 @@ namespace Infrastructure.Validations;
 
 public class UpdateValidation : AbstractValidator<UpdateCustomerDTO>
 {
+    public UpdateValidation()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("The inserted Id is incorrect");
+
+        RuleFor(create => create.FirstName)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Name can only contain letters, dots or spaces.")
+            .Length(3, 40);
+
+        RuleFor(create => create.LastName)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Name can only contain letters, dots or spaces.")
+            .Length(3, 40);
+
+        RuleFor(create => create.Email)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("The email must have an at symbol");
+
+        RuleFor(create => create.Phone)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("The phone number must have at least 10 numbers");
+
+        RuleFor(create => create.BirthDate)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("The date format has to be yyyy/mm/dd");
+    }
 }
